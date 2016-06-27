358301da74ac38c3b4ad78d99fea9bc03660063d
Andrew Gardhouse <andrew.gardhouse@invokemedia.com>
2016-06-27 11:37:56 -0700

    When a user clicks on a notification, a URL is opened in another tab

--- service-worker.js
+++ service-worker.js
@@ -17,3 +17,23 @@ self.addEventListener('push', function(event) {
     })
   );
 });
+
+self.addEventListener('notificationclick', function(event) {
+  event.notification.close();
+  var url = "https://www.invokemedia.com/"
+  event.waitUntil(
+    clients.matchAll({
+      type: 'window'
+    }).then(function(windowClients) {
+      for (var i = 0; i < windowClients.length; i++) {
+        var client = windowClients[i];
+        if (client.url === url && 'focus' in client) {
+          return client.focus()
+        }
+      }
+      if (clients.openWindow) {
+        return clients.openWindow(url)
+      }
+    })
+  )
+});
